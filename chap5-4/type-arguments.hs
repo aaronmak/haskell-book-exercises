c3a36da1e90bb8313e65fe1c037ccd943f9fbe11
Aaron Mak <aaronmakks@gmail.com>
2019-07-01 07:06:45 +0800

    Add chap 5-4

---
+++ chap5-4/type-arguments.hs
@@ -0,0 +1,22 @@
+main :: IO ()
+
+-- 1: Char -> Char -> Char
+
+-- 2: Char
+
+-- 3: Num b => b
+
+-- 4: Double
+
+-- 5: [Char]
+
+-- 6: Eq b => b -> [Char]
+
+-- 7: (Ord a, Num a) => a
+-- not sure why the answer is differnt
+
+-- 8: (Ord a, Num a) => a
+
+-- 9: Integer
+
+main = print "foo"
