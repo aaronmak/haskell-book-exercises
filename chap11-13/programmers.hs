8ebd06b2fcb0d3451412652a5de64beac9b2979a
Aaron Mak <aaronmakks@gmail.com>
2019-09-06 12:15:13 +0800

    Add chap11-13

---
+++ chap11-13/programmers.hs
@@ -0,0 +1,46 @@
+main :: IO ()
+
+data OperatingSystem =
+      GnuPlusLinux
+    | OpenBSDPlusNevermindJustBSDStill
+    | Mac
+    | Windows
+    deriving (Eq, Show)
+
+data ProgLang =
+      Haskell
+    | Agda
+    | Idris
+    | PureScript
+    deriving (Eq, Show)
+
+data Programmer =
+  Programmer { os :: OperatingSystem
+            , lang :: ProgLang }
+  deriving (Eq, Show)
+
+allOperatingSystems :: [OperatingSystem]
+allOperatingSystems =
+  [ GnuPlusLinux
+  , OpenBSDPlusNevermindJustBSDStill
+  , Mac
+  , Windows
+  ]
+
+allLanguages :: [ProgLang]
+allLanguages =
+  [Haskell, Agda, Idris, PureScript]
+
+allProgrammers :: [Programmer]
+allProgrammers =
+  [ Programmer
+    { os = o
+    , lang = l
+    } | o <- allOperatingSystems
+      , l <- allLanguages
+  ]
+
+
+main = do
+  print allProgrammers
+  print $ length allProgrammers
