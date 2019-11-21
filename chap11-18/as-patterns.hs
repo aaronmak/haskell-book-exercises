e3648a9b200b5ca36c27a656a6e1959b215453a9
Aaron Mak <aaronmakks@gmail.com>
2019-11-21 14:43:24 +0800

    Complete chap11-18/as-patterns.hs

--- chap11-18/as-patterns.hs
+++ chap11-18/as-patterns.hs
@@ -14,7 +14,17 @@ isSubseqOf t@(x:xs) (y:ys)
   | x == y = True && isSubseqOf xs ys
   | otherwise = isSubseqOf t ys
 
+
+capitalizeWord :: String
+               -> (String, String)
+capitalizeWord w@(x:xs) = (w, toUpper x:xs)
+
+capitalizeWords :: String
+                -> [(String, String)]
+capitalizeWords xs = map capitalizeWord $ words xs
+
 main = do
   print $ isSubseqOf "blah" "wblaah" == True
   print $ isSubseqOf "blah" "wootbla" == False
   print $ isSubseqOf "blah" "halb" == False
+  print $ capitalizeWords "hello world"
