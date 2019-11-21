0714dc8a8a632a8a8df851c5e1f2ac887157d92c
Aaron Mak <aaronmakks@gmail.com>
2019-11-21 15:46:15 +0800

    Add chap11-18/language-exercises.hs

---
+++ chap11-18/language-exercises.hs
@@ -0,0 +1,29 @@
+import Data.Char
+import Data.List
+
+main :: IO ()
+
+capitalizeWord :: String -> String
+
+capitalizeWord w@(x:xs)
+  | x == ' ' = x : capitalizeWord xs
+  | x == toUpper x = w
+  | otherwise = toUpper x : xs
+
+capitalizeParagraph :: String -> String
+capitalizeParagraph xs =
+  foldl (++) []
+  $ map capitalizeWord
+  $ splitByPeriod xs
+
+isSentenceBreak :: Char -> Char -> Bool
+isSentenceBreak x y = (x /= '.' && y /= '.')
+
+splitByPeriod :: String -> [String]
+splitByPeriod xs = groupBy isSentenceBreak xs
+
+main = do
+  print $ capitalizeWord "foo" == "Foo"
+  print $ capitalizeWord "Foo" == "Foo"
+  print $ splitByPeriod "blah. woot ha."
+  print $ capitalizeParagraph "blah. woot ha."
