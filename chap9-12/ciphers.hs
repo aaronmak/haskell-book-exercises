c01b42ca1c2d9a10bf5299bc222049b8063b1ae9
Aaron Mak <aaronmakks@gmail.com>
2020-09-06 13:54:25 +0800

    Validate ciphers using quickcheck

--- chap9-12/ciphers.hs
+++ chap9-12/ciphers.hs
@@ -1,24 +1,36 @@
 module Cipher where
 
 import Data.Char
+import Test.Hspec
+import Test.QuickCheck
 
 caesar :: Char -> Int -> Char
-caesar char n = chr $ mod (ord char + n - ord 'a') 26 + ord 'a'
+caesar char n
+  | ord char < 65 = char
+  | n < 0 = char
+caesar char n = chr $ ord char + n
 
 unCaesar :: Char -> Int -> Char
-unCaesar char n = chr $ mod (ord char - n - ord 'a') 26 + ord 'a'
+unCaesar char n
+  | ord char < 65 = char
+  | n < 0 = char
+unCaesar char n = chr $ ord char - n
 
 caesarMap :: [Char] -> Int -> [Char]
 caesarMap [] n = []
 caesarMap (x:xy) n = [caesar x n] ++ caesarMap xy n
 
 
--- Chap 13-14 Modifying code Q1
 main :: IO ()
-main = do
-  putStrLn "What would you like to encode?"
-  toEncode <- getLine
-  putStrLn "By how many steps?"
-  encryptSteps <- getLine
-  let n = (read encryptSteps :: Int)
-  putStrLn $ caesarMap toEncode n
+main = hspec $ do
+  describe "Encode and decoding any character" $ do
+    it "should give the same result" $ do
+      property $ \x y -> unCaesar (caesar x y) y == x
+
+-- Chap 13-14 Modifying code Q1
+  -- putStrLn "What would you like to encode?"
+  -- toEncode <- getLine
+  -- putStrLn "By how many steps?"
+  -- encryptSteps <- getLine
+  -- let n = (read encryptSteps :: Int)
+  -- putStrLn $ caesarMap toEncode n
